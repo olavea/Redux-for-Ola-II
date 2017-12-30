8f9982c33ef955364285ac8322295c2dea92b0cc
Ola Vea <ola@olavea.com>
2017-12-30 10:09:18 +0100

    video 12: Updating State with Reducers
    Run 2 Wes Bos video 12: Updating State with Reducers
    12 / 20
    Once the action has been dispatched, we can use reducers to update our state. 
    // I don't want comments or likes on Lilliangram

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -21,9 +21,21 @@
 // if something is going to happen in state is up to you, but first see something
 
 function posts(state = [], action) {
-  console.log("The post will change");
-  console.log(state, action);
-  return state;
+  // console.log("The post will change");
+  // console.log(state, action);
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log("Incrementing Likes!!");
+      const i = action.index;
+      return [
+        ...state.slice(0,i), // before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), // after the one we are updating
+      ]
+    default:
+      return state;
+  }
 }
+
 
 export default posts;
