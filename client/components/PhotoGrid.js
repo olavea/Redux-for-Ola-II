7782d9dea6c05d7809fc71358c66b09bdf07b693
Ola Vea <ola@olavea.com>
2017-12-30 10:02:24 +0100

    video 11: Displaying Redux State inside our Components
    Run 2 Wes Bos video 11: Displaying Redux State inside our Components
    11 / 20
    Let's take a break from actions, action creators and reducers for a second a see how we can pull our state into the components at the level we wish.

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
-      I'm the photo grid
+        {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
+        // this.props is an array we are gping to map over it
+        // Each child should have a unique "key" prop Check the render method, illuSketchNote
       </div>
     )
   }
