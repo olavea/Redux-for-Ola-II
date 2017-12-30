cd7e5a84e37dc2b57db7e02a6c20af547690d637
Ola Vea <ola@olavea.com>
2017-12-30 14:01:48 +0100

    video 19: Learning Redux Dev Tools
    Run 2 Wes Bos video 19: Learning Redux Dev Tools
    19 / 20
    Learn all about the power of the Redux dev tools to inspect and time travel with your actions. 
    // I don't want comments or likes on Lilliangram
    
    
    
    https://github.com/olavea/Redux-for-Ola/blob/master/client/store.js

--- client/store.js
+++ client/store.js
@@ -15,9 +15,18 @@ const defaultState = {
   // comments //ES6
   // I don't want comments or likes on Lilliangram
 };
+const enhancers = compose (
+  // if there is devTools ? we will rin it devTools() otherwise : we will simply return the store itself
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
 
-const store = createStore(rootReducer, defaultState);
+// Error, in console gotta spell compose right on line 1
+// Sweep away actions from ... is like they never happened
+// commit like a git commit to our initial state devToolsExtension
+// revert back to before "stupid dog" comment
+// reset all the way back to our initial state devToolsExtension
 
+const store = createStore(rootReducer, defaultState, enhancers);
 export const history = syncHistoryWithStore(browserHistory, store);
 // we take the browserHistory and we are going to weave in the actual store
 
