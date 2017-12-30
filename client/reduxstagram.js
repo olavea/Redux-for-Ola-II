b72f42bf2477ae50c1dbd6aa027842f4f42c2127
Ola Vea <ola@olavea.com>
2017-12-30 09:37:06 +0100

    video 8: Integrating our Store with React Router
    Run 2 Wes Bos video 8: Integrating our Store with React Router
    Let's get everyone on the same page and let React Router know about our store. 
    // I don't want comments or likes on Lilliangram

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -12,16 +12,22 @@ import PhotoGrid from './components/PhotoGrid';
 
 // import react router deps
 import { Router, Routing, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
+// expose our store to our actual aplication here
+// wrap the entire router itself in a provider tag
+// swap out browserHistory with history
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute> 
-  //They are going to be children of the Main componenet
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'));
